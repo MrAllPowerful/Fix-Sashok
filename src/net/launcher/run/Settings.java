df5c7f451d93c10030eb58eae67996d65c816bc5
zenit <alexandrage@mail.ru>
2013-10-01 14:23:50 +0400

    liteloader 1.6.4

--- src/net/launcher/run/Settings.java
+++ src/net/launcher/run/Settings.java
@@ -7,18 +7,18 @@ package net.launcher.run;
 public class Settings
 {
 	/** Настройка заголовка лаунчера */
-	public static final String 		title		 	 = "Launcher"; //Заголовок лаунчера
-	public static final String 		titleInGame  	 = "Minecraft"; //Заголовок лаунчера после авторизации
-	public static final String 		basedir			 = "AppData"; //Родительская папка для Minecraft (только для Windows) [ProgramFiles, AppData]
-	public static final String 		baseconf		 = "voxelaria"; //Папка с файлом конфигурации
-	public static final String		pathconst		 = "voxelaria/%SERVERNAME%"; //Конструктор пути к папке с MC
-	public static final String      skins            = "MinecraftSkins/"; //Папка скинов
-	public static final String      cloaks           = "MinecraftCloaks/"; //Папка плащей
+	public static final String  title		 = "Launcher"; //Заголовок лаунчера
+	public static final String  titleInGame  	 = "Minecraft"; //Заголовок лаунчера после авторизации
+	public static final String  basedir	         = "AppData"; //Родительская папка для Minecraft (только для Windows) [ProgramFiles, AppData]
+	public static final String  baseconf		 = "voxelaria"; //Папка с файлом конфигурации
+	public static final String  pathconst		 = "voxelaria/%SERVERNAME%"; //Конструктор пути к папке с MC
+	public static final String  skins                = "MinecraftSkins/"; //Папка скинов
+	public static final String  cloaks               = "MinecraftCloaks/"; //Папка плащей
 	/** Параметры подключения */
-	public static final String 	domain	 	 	     = "alexandrage.dyndns.org/T";//Домен сайта
-	public static final String  siteDir		  	     = "site";//Папка с файлами лаунчера на сайте
-	public static final String  updateFile		     = "http://alexandrage.dyndns.org/T/site/Launcher.jar";//Ссылка на файл обновления лаунчера
-	public static final String 	buyVauncherLink      = "http://plati.ru/"; //Ссылка на страницу покупки ваучеров
+	public static final String  domain	 	 = "alexandrage.dyndns.org/T";//Домен сайта
+	public static final String  siteDir		 = "site";//Папка с файлами лаунчера на сайте
+	public static final String  updateFile		 = "http://alexandrage.dyndns.org/T/site/Launcher.jar";//Ссылка на файл обновления лаунчера
+	public static final String  buyVauncherLink      = "http://plati.ru/"; //Ссылка на страницу покупки ваучеров
 	
 	/** Для одиночной игры */
 	public static final String  defaultUsername      = "player"; //Имя пользователя для одиночной игры
@@ -29,14 +29,16 @@ public class Settings
 	// 1-> Имя папки клиента 2-> ip 3-> port 
 	// 4-> Версия клиента для автопатча директории (старые версии до 1.5.2)
 	// 5-> Тип запуска клиента 1 для старых версий 2 для новых
-	// 6-> 1 для запуска чистого клиента
-	//     2 для запуска клиента с forge без Liteloader
-	//     3 для запуска клиента с Liteloader и Liteloader+forge
+	// 6-> 1 для запуска чистого клиента 1.6.2-1.6.4
+	//     2 для запуска клиента с forge без Liteloader 1.6.2-1.6.4
+	//     3 для запуска клиента с Liteloader и Liteloader+forge 1.6.2
+	//     4 для запуска клиента с Liteloader и Liteloader+forge 1.6.4
 	public static final String[] servers =
 	{
 		"v, localhost, 25565, 1.5.x, 1, none",
 		"vanila, localhost, 25565, none, 2, 1",
 		"voxelaria, localhost, 25565, none, 2, 3",
+		"voxelaria4, localhost, 25565, none, 2, 4",
 	};
 
 	/** Настройка панели ссылок **/
@@ -49,20 +51,20 @@ public class Settings
 	/** Настройки структуры лаунчера */
 	public static boolean useAutoenter	         =  false; //Использовать функцию автозахода на выбранный сервер
 	
-	public static boolean useRegister		 	 =  false; //Использовать Регистрацию в лаунчере
+	public static boolean useRegister		 =  false; //Использовать Регистрацию в лаунчере
 	
 	public static boolean useMulticlient		 =  true; //Использовать функцию "по клиенту на сервер"
-	public static boolean useStandartWB		     =  true; //Использовать стандартный браузер для открытия ссылок
-	public static boolean usePersonal		 	 =  false; //Использовать Личный кабинет
-	public static boolean customframe 		     =  true; //Использовать кастомный фрейм
-	public static boolean useOffline 			 =  true; //Использовать режим оффлайн
+	public static boolean useStandartWB		 =  true; //Использовать стандартный браузер для открытия ссылок
+	public static boolean usePersonal		 =  false; //Использовать Личный кабинет
+	public static boolean customframe 		 =  true; //Использовать кастомный фрейм
+	public static boolean useOffline 	         =  true; //Использовать режим оффлайн
 	public static boolean useConsoleHider		 =  true; //Использовать скрытие консоли клиента
 	public static boolean useModCheckerTimer	 =  true; //Каждые 30 секунд моды будут перепроверяться
 
 	public static final String protectionKey	 = "1234567890"; //Ключ защиты сессии. Никому его не говорите.
 
-	public static boolean debug		 	         =  false; //Отображать все действия лаунчера (отладка)(true/false)
-	public static boolean drawTracers		     =  false; //Отрисовывать границы элементов лаунчера
+	public static boolean debug		 	 =  false; //Отображать все действия лаунчера (отладка)(true/false)
+	public static boolean drawTracers		 =  false; //Отрисовывать границы элементов лаунчера
 	public static final String masterVersion  	 = "final_RC4"; //Версия лаунчера
 
 	public static boolean patchDir 		         =  true; //Использовать автоматическую замену директории игры (true/false)

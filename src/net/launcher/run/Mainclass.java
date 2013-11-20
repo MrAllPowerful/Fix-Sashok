91efe8685f3e573eb3cde2f59ff18d93377ee90f
zveronline <zveronline@localhost.localdomain>
2013-11-21 02:41:40 +0400

    добавлена возможность отключения музыки "на ходу"

--- src/net/launcher/run/Mainclass.java
+++ src/net/launcher/run/Mainclass.java
@@ -2,14 +2,15 @@ package net.launcher.run;
 
 import net.launcher.MusPlay;
 import net.launcher.components.Frame;
+import net.launcher.utils.BaseUtils;
 
 public class Mainclass
 {
 	public static void main(String[] args) throws Exception
 	{	
 		Frame.start();
-		if(Settings.Music)
-		{	
+		if(BaseUtils.getPropertyBoolean("Music", true))
+		{
                MusPlay mp3 = new MusPlay(Settings.iMusicname);
                mp3.play();}
 	}

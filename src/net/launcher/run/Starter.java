2edccb68a4a19b956324ac1dd8a80aa118764c3f
zenit <zenit@m.ru>
2014-04-18 20:35:17 +0400

    -Xdock:icon=favicon.png

--- src/net/launcher/run/Starter.java
+++ src/net/launcher/run/Starter.java
@@ -1,5 +1,9 @@
 package net.launcher.run;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 
 import net.launcher.utils.BaseUtils;
@@ -8,17 +12,37 @@ public class Starter
 {
 	public static void main(String[] args) throws Exception
 	{	
-		try
-		{
+		
+		InputStream stream = Starter.class.getResourceAsStream("/net/launcher/theme/favicon.png");
+	    if (stream == null) {
+	    }
+	    OutputStream resStreamOut = null;
+	    int readBytes;
+	    byte[] buffer = new byte[4096];
+	    try {
+	        resStreamOut = new FileOutputStream(new File(BaseUtils.getAssetsDir().toString()+"/favicon.png"));
+	        while ((readBytes = stream.read(buffer)) > 0) {
+	            resStreamOut.write(buffer, 0, readBytes);
+	        }
+	    } catch (IOException e1) {
+	        e1.printStackTrace();
+	    } finally {
+	        stream.close();
+	        resStreamOut.close();
+	    }
+		
+		try {
+
 			String jarpath = Starter.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
 			int memory = BaseUtils.getPropertyInt("memory", 512);
 			
 			ArrayList<String> params = new ArrayList<String>();
-
+           
 			if (BaseUtils.getPlatform() == 2) params.add("javaw"); else params.add("java");
 			params.add("-Xmx" + memory + "m");
 			params.add("-Xms" + memory + "m");
 			params.add("-Xdock:name=Minecraft");
+			params.add("-Xdock:icon=" + BaseUtils.getAssetsDir().toString()+"/favicon.png");
 			params.add("-Dsun.java2d.noddraw=true");
 			params.add("-Dsun.java2d.d3d=false");
 			params.add("-Dsun.java2d.opengl=false");

718c0241f87e5db2bf900729af373b5cf15b3362
zenit <zenit@m.ru>
2014-04-19 16:19:19 +0400

    .

--- site/servers.php
+++ site/servers.php
@@ -12,8 +12,8 @@ voxelaria162, localhost, 25564, 1.6.2<::>
 voxelaria164, localhost, 25564, 1.6.4<::>
 spoutcraft164, localhost, 25565, 1.6.2<::>
 voxelaria172, localhost, 25565, 1.7.2<::>
-vanilla174, localhost, 25565, 1.7.3<::>
-Test, 212.83.147.47, 25815, 1.6.4<::>
+vanilla175, localhost, 25565, 1.7.4<::>
+vanilla179, localhost, 25565, 1.7.4<::>
 '
 ;
 // Для версий 1.7.3 и 1.7.4 обязательно нужно указать 1.7.3. Иначе не будет читаться папка assets

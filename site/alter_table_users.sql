13778d1ac0a04c93b43e358abce68610afbab83e
zenit <zenit@m.ru>
2014-04-28 17:49:49 +0400

    .

--- site/alter_table_users.sql
+++ site/alter_table_users.sql
@@ -1,5 +1,3 @@
 ﻿ALTER TABLE  `Таблица пользователей`
 ADD  `realmoney` int(255) DEFAULT '0',
-ADD  `md5` varchar(255) DEFAULT '0',
-ADD  `session` varchar(255) DEFAULT '0',
-ADD  `server` varchar(255) DEFAULT '0'
+ADD  `md5` varchar(255) DEFAULT '0'
